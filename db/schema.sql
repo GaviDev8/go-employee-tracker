2aeefddb6b33d4a0b962bb1d4e5f081574386a4b
Gabriela Ortiz <gabrielaortiz@Gabrielas-Air.lan>
2024-04-28 20:26:01 -0700

    setting up file structure, database name, and table structure

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+\c employee_tracker_db;
+
+CREATE TABLE department_mt (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    FOREIGN KEY (department)
+    REFERENCES department_mt(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE   ('Gabriela', 'Ortiz', '1', '99'),
+        ('Spongebob', 'Squarepants', '2', '99'),
+        ('Scooby', 'Doo', '3', '99'),
+        ('Timmy', 'Turner', '4', '99'),
+        ('Betty', 'Bop', '5', '100'),
+
+INSERT INTO role (title, salary)
+VALUE
