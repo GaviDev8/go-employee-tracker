eba6a0ec8e6ebf70ab44050346cce686c0544f5e
Gabriela Ortiz <gabrielaortiz@Gabrielas-Air.lan>
2024-05-01 22:24:35 -0700

    need 8 more functions

--- server.js
+++ server.js
@@ -117,7 +117,140 @@ function viewEmployeesByManager() {
     });
 }
 
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter first name:'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter last name:'
+        },
+    ]).then(({ first_name, last_name }) => {
+        pool.query("SELECT * FROM role", (err, results) => {
+            if (err) {
+                console.error('Error fetching roles:', err);
+                return;
+            }
+            const roleChoices = results.rows.map(({ id, title }) => ({
+                name: title,
+                value: id,
+            }));
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'roleId',
+                    message: "What is employee's role?",
+                    choices: roleChoices
+                }
+            ]).then(({ roleId }) => {
+                pool.query("SELECT * FROM employee", (err, results) => {
+                    if (err) {
+                        console.error('Error fetching employees:', err);
+                        return;
+                    }
+                    let managerChoices = results.rows.map(({ id, first_name, last_name }) => ({
+                        name: `${first_name} ${last_name}`,
+                        value: id,
+                    }));
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'managerId',
+                            message: "Who is the employee's manager?",
+                            choices: managerChoices
+                        }
+                    ]).then(({ managerId }) => {
+                        let employee = {
+                            manager_id: managerId,
+                            role_id: roleId,
+                            first_name: first_name,
+                            last_name: last_name
+                        }
 
+                        let sqlQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`;
+                        pool.query(sqlQuery, [employee.first_name, employee.last_name, employee.role_id, employee.manager_id], (err, result) => {
+                            if (err) {
+                                console.error('Error adding employee:', err);
+                                return;
+                            }
+                            console.log(`Employee added: ${employee.first_name} ${employee.last_name}`);
+                            loadMainMenu();
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+function removeEmployee() {
+    let sqlQuery = `SELECT * FROM employee`;
+    pool.query(sqlQuery, (err, results) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const employeeChoices = results.rows.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id,
+        }));
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'employeeId',
+                message: 'Select the employee to remove:',
+                choices: employeeChoices
+            }
+        ]).then(({ employeeId }) => {
+            const deleteQuery = 'DELETE FROM employee WHERE id = $1';
+            pool.query(deleteQuery, [employeeId], (err, result) => {
+                if (err) {
+                    console.error('Error removing employee:', err);
+                    return;
+                }
+                console.log('Employee removed successfully.');
+                loadMainMenu();
+            });
+        });
+    });
+}
+
+
+function updateEmployeeRole() {
+
+}
+
+function updateEmployeeManager() {
+    
+}
+
+function addDepartment() {
+    
+}
+
+function removeDepartment() {
+    
+}
+
+function viewUtilizeBudgetByDepartment() {
+    
+}
+
+function viewRole() {
+    
+}
+
+function addRole() {
+    
+}
+
+function removeRole() {
+    
+}
 // main menu function/prompt
 function loadMainMenu() {
     inquirer.prompt([{
@@ -135,6 +268,12 @@ function loadMainMenu() {
                 name: "View all employees by manager",
                 value: "VIEW_EMPLOYEES_BY_MANAGER",
             },{
+                name: "Add employee",
+                value: "ADD_EMPLOYEE",
+            },{
+                name: "Remove employee",
+                value: "REMOVE_EMPLOYEE"
+            },{
                 name: "Quit",
                 value: "QUIT"
             }
@@ -148,7 +287,13 @@ function loadMainMenu() {
         }
         else if (choice === "VIEW_EMPLOYEES_BY_MANAGER") {
             viewEmployeesByManager();
-        }
+        } 
+        else if (choice === "ADD_EMPLOYEE") {
+            addEmployee();
+        } 
+        else if (choice === "REMOVE_EMPLOYEE") {
+            removeEmployee();
+        } 
         else {
             quit();
         }
